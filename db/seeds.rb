e37b9c36c27d7b8a9f95ac87abdd2e09a36a77c1
Felipe Manhani <fbmanhani@gmail.com>
2018-09-06 16:22:02 -0300

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+categories = ["Animais e acessórios", "Esportes", "Para a sua casa", "Eletrônicos e celulares", "Músicas e hobbies",
+              "Bebês e crianças", "Moda e beleza", "Veículos e barcos", "Imóveis", "Empregos e negócios"]
+
+puts "Cadastrando categorias..."
+categories.each do |item|
+  Category.find_or_create_by!(description: item)
+end
+puts "Cadastrando categorias...OK"
