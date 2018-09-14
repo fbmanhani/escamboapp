244dab0282b88f9805845f32d1aca183bc32f9d7
Felipe Manhani <fbmanhani@gmail.com>
2018-09-14 10:59:27 -0300

    Layout adjustments - Section 3 - Lesson 123

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+
+  get 'backoffice', to: 'backoffice/dashboard#index'
+  namespace :backoffice do
+    resources :categories, except: [:show, :destroy]
+    get 'dashboard', to: 'dashboard#index'
+  end
+
   namespace :site do
     get 'home', to: 'home#index'
   end
 
-  namespace :backoffice do
-    get 'dashboard', to: 'dashboard#index'
-  end
 
   devise_for :admins
   devise_for :members
